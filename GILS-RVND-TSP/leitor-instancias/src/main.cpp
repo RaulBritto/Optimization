4a6ae6e8e27ffe2a2b633b78dd7c7a406edb8ae0
Raul Britto <raul.britto08@gmail.com>
2019-12-08 19:22:38 -0300

    Nova struct

--- GILS-RVND-TSP/leitor-instancias/src/main.cpp
+++ GILS-RVND-TSP/leitor-instancias/src/main.cpp
@@ -17,7 +17,7 @@ vector<double> R = {0.0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0
                     , 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.20, 0.21
                     , 0.21, 0.22, 0.23, 0.24, 0.25};
 //TODO change to IMAX                    
-int IMAX = 50;  //number of iteration
+int IMAX = 1;  //number of iteration
 /*This struct helps the construction step*/
 struct InsertionInfo{
   int insertedNode; // node k to be inserted
@@ -26,6 +26,12 @@ struct InsertionInfo{
 
   bool operator() (InsertionInfo i,InsertionInfo j) { return (i.cost<j.cost);}
 };
+
+struct solution{
+  vector<int> cities;
+  double distance; //delta after insert the node k in the edge {i,j}
+};
+
 /*This function print all the adjacency matrix*/
 void printData() {
   cout << "dimension: " << dimension << endl;
@@ -111,26 +117,26 @@ vector<int>  Construction(double &alpha){
 }
 
 /*This function calculates all the possibles 'swap' between the nodes and chooses the best one (Descresing the total distance).*/
-int Swap(vector<int> &s, double distancia){
+void Swap(solution &s){
 
   double delta = 0, deltaMinimium = 0, deltaFixed = 0;
   int firstNode = 0, secondNode = 0, length = 0;
 
-    length = s.size();
+    length = s.cities.size();
     for(int i = 1; i < length-2; i++){
-      deltaFixed = - matrizAdj[s[i-1]][s[i]] - matrizAdj[s[i]][s[i+1]];   
+      deltaFixed = - matrizAdj[s.cities[i-1]][s.cities[i]] - matrizAdj[s.cities[i]][s.cities[i+1]];   
       for(int j = i+1; j < length-1; j++){  
         if(j == i+1){
-          delta = deltaFixed - matrizAdj[s[j]][s[j+1]]
-                    + matrizAdj[s[i-1]][s[j]] + matrizAdj[s[j]][s[i]] + matrizAdj[s[i]][s[j+1]];
+          delta = deltaFixed - matrizAdj[s.cities[j]][s.cities[j+1]]
+                    + matrizAdj[s.cities[i-1]][s.cities[j]] + matrizAdj[s.cities[j]][s.cities[i]] + matrizAdj[s.cities[i]][s.cities[j+1]];
           if(deltaMinimium > delta){
             deltaMinimium = delta;
             firstNode = i; secondNode = j;  
           }     
         } 
         else{
-          delta = deltaFixed - matrizAdj[s[j-1]][s[j]] - matrizAdj[s[j]][s[j+1]]
-                  + matrizAdj[s[i-1]][s[j]] + matrizAdj[s[j]][s[i+1]] + matrizAdj[s[j-1]][s[i]] + matrizAdj[s[i]][s[j+1]];
+          delta = deltaFixed - matrizAdj[s.cities[j-1]][s.cities[j]] - matrizAdj[s.cities[j]][s.cities[j+1]]
+                  + matrizAdj[s.cities[i-1]][s.cities[j]] + matrizAdj[s.cities[j]][s.cities[i+1]] + matrizAdj[s.cities[j-1]][s.cities[i]] + matrizAdj[s.cities[i]][s.cities[j+1]];
           if(deltaMinimium > delta){
             deltaMinimium = delta;
             firstNode = i; secondNode = j;  
@@ -140,24 +146,23 @@ int Swap(vector<int> &s, double distancia){
     }
     //Check if exists an improvement
     if(deltaMinimium < 0){
-      distancia = distancia + deltaMinimium;
-      swap(s[firstNode], s[secondNode]);
+      s.distance += deltaMinimium;
+      swap(s.cities[firstNode], s.cities[secondNode]);
     }
-      return distancia;      
 }
 /*This function calculates all the reverse subsenquences in the solution and chooses the best one (Descresing the total distance).*/
-int _2opt(vector<int> &s, double distancia){
+void _2opt(solution &s){
 
   double delta = 0, deltaMinimium = 0, deltaFixed = 0;
   int firstNode = 0, secondNode = 0, length = 0;
   vector<int> subsequence;
 
-    length = s.size();
+    length = s.cities.size();
     for(int i = 1 ; i < length-2; i++){
-      deltaFixed = - matrizAdj[s[i-1]][s[i]];
+      deltaFixed = - matrizAdj[s.cities[i-1]][s.cities[i]];
       for(int j = i +1; j < length-1; j++){
-        delta = deltaFixed - matrizAdj[s[j]][s[j+1]]
-                  + matrizAdj[s[i-1]][s[j]] + matrizAdj[s[i]][s[j+1]];
+        delta = deltaFixed - matrizAdj[s.cities[j]][s.cities[j+1]]
+                  + matrizAdj[s.cities[i-1]][s.cities[j]] + matrizAdj[s.cities[i]][s.cities[j+1]];
         if(deltaMinimium > delta){
           deltaMinimium = delta;
           firstNode = i; secondNode = j;  
@@ -167,33 +172,30 @@ int _2opt(vector<int> &s, double distancia){
     //Check if exists an improvement
     if(deltaMinimium < 0){
       //creting new reverse sequence 
-      for(int i = secondNode; i >= firstNode; i--) subsequence.push_back(s[i]);
+      for(int i = secondNode; i >= firstNode; i--) subsequence.push_back(s.cities[i]);
       //replacing the sequence with the old one
-      s.insert(s.begin()+ firstNode , subsequence.begin(),subsequence.end());
-      s.erase(s.begin() + firstNode + subsequence.size()  , s.begin() + firstNode + 2*subsequence.size());
+      s.cities.insert(s.cities.begin()+ firstNode , subsequence.begin(),subsequence.end());
+      s.cities.erase(s.cities.begin() + firstNode + subsequence.size()  , s.cities.begin() + firstNode + 2*subsequence.size());
       //updating the new distance after moving the sequence
-      distancia = distancia + deltaMinimium;
-      subsequence.clear();
+      s.distance +=  deltaMinimium;
     }
-
-  return distancia; 
 }
 
 /*This function calculates the best insertion of a subsequence of size 'k' in the solution and chooses the best one (Descresing the total distance).*/
-int orkOpt(vector<int> &s, double distancia, int k){
+void orkOpt(solution &s, int k){
 
   vector<int> subsequence;
   double delta = 0, deltaMinimium = 0, deltaFixed = 0;
   int firstNode = 0, secondNode = 0, length = 0;
 
-    length = s.size();
+    length = s.cities.size();
     for(int i = 1; i < length -k; i++){
-      deltaFixed = - matrizAdj[s[i-1]][s[i]] - matrizAdj[s[i+k-1]][s[i+k]] + matrizAdj[s[i-1]][s[i+k]];
+      deltaFixed = - matrizAdj[s.cities[i-1]][s.cities[i]] - matrizAdj[s.cities[i+k-1]][s.cities[i+k]] + matrizAdj[s.cities[i-1]][s.cities[i+k]];
       //cout << deltaFixed << endl;
       for(int j = 0; j < length -1; j++){
         if(j >= i-1 && j < i+k) continue;
         else{
-          delta = deltaFixed + matrizAdj[s[j]][s[i]] + matrizAdj[s[i+k-1]][s[j+1]] - matrizAdj[s[j]][s[j+1]];
+          delta = deltaFixed + matrizAdj[s.cities[j]][s.cities[i]] + matrizAdj[s.cities[i+k-1]][s.cities[j+1]] - matrizAdj[s.cities[j]][s.cities[j+1]];
           if(deltaMinimium > delta){
             deltaMinimium = delta;
             firstNode = i; secondNode = j;  
@@ -206,33 +208,30 @@ int orkOpt(vector<int> &s, double distancia, int k){
     if(deltaMinimium < 0){
       //Creating the subsequence that will be moved  
       for(int j = 0; j < k; j++ ){
-        subsequence.push_back(s[j+firstNode]);
+        subsequence.push_back(s.cities[j+firstNode]);
       }
-      s.erase(s.begin()+firstNode, s.begin()+firstNode+k);  
+      s.cities.erase(s.cities.begin()+firstNode, s.cities.begin()+firstNode+k);  
       if(firstNode < secondNode){
-        s.insert(s.begin()+secondNode+1-k, subsequence.begin(), subsequence.end());
+        s.cities.insert(s.cities.begin()+secondNode+1-k, subsequence.begin(), subsequence.end());
       }else{
-        s.insert(s.begin()+secondNode+1, subsequence.begin(), subsequence.end());  
+        s.cities.insert(s.cities.begin()+secondNode+1, subsequence.begin(), subsequence.end());  
       }
-      distancia = distancia + deltaMinimium;
-
+      s.distance += deltaMinimium;
     } 
-
-  return distancia; 
 }
  /*This function pertubs the solution (chooses two subsequences with random size with no matching and swaps both).*/
-vector<int> Pertub(vector<int> s){
+solution Pertub(solution s){
 
   vector<int> subsequence;
   std::vector<int>::iterator it;
   int index;
 
   //Choose the first index
-  index = (rand() % (s.size()-2)) + 1;
+  index = (rand() % (s.cities.size()-2)) + 1;
   //Choose the next three indexs randomly
   for(int i = 0; i <= 3; i++){
     do{
-      index = (rand() % (s.size()-2)) + 1;
+      index = (rand() % (s.cities.size()-2)) + 1;
       it = find (subsequence.begin(), subsequence.end(), index);
     }while(it != subsequence.end());
     subsequence.push_back(index);
@@ -240,58 +239,55 @@ vector<int> Pertub(vector<int> s){
   std::sort (subsequence.begin(), subsequence.end());
   if(subsequence[1] - subsequence[0] > 10) subsequence[1] = subsequence[0]+10;
   if(subsequence[3] - subsequence[2] > 10) subsequence[3] = subsequence[2]+10;
+
   //Swap the subvectors sequences
-  vector<int> subvector1 = std::vector<int>(s.begin()+subsequence[0],s.begin()+subsequence[1]+1);
-  vector<int> subvector2 = std::vector<int>(s.begin()+subsequence[2],s.begin()+subsequence[3]+1);
-  s.insert(s.begin()+ subsequence[2] , subvector1.begin(),subvector1.end());
-  s.erase(s.begin() + subsequence[2] + subvector1.size(), s.begin() + subsequence[3]+1 + + subvector1.size());
-  s.erase(s.begin() + subsequence[0], s.begin() + subsequence[1]+1);
-  s.insert(s.begin()+ subsequence[0] , subvector2.begin(),subvector2.end());
+  vector<int> subvector1 = std::vector<int>(s.cities.begin()+subsequence[0],s.cities.begin()+subsequence[1]+1);
+  vector<int> subvector2 = std::vector<int>(s.cities.begin()+subsequence[2],s.cities.begin()+subsequence[3]+1);
+  s.cities.insert(s.cities.begin()+ subsequence[2] , subvector1.begin(),subvector1.end());
+  s.cities.erase(s.cities.begin() + subsequence[2] + subvector1.size(), s.cities.begin() + subsequence[3]+1  + subvector1.size());
+  s.cities.erase(s.cities.begin() + subsequence[0], s.cities.begin() + subsequence[1]+1);
+  s.cities.insert(s.cities.begin()+ subsequence[0] , subvector2.begin(),subvector2.end());
 
   return s;
 }
 
 /*This function implements the Random VND and return the minimum distance found.*/
-int RVND(vector<int> &s){
+void RVND(solution &s){
   
   vector<int> NL;
-  vector<int> s_;
-  int distance = 0, distance_ = 0, choosed = 0;
+  solution s_;
   //set some values:
   for (int i=1; i<=5; ++i) NL.push_back(i);
   //using built-in random generator:
   random_shuffle(NL.begin(), NL.end());
 
   s_ = s;
-  distance = getDistance(s);
-  distance_ = distance;
 
   while(!NL.empty()){
     switch (NL.front()){ 
       case 1:
         //cout << "Swap" << endl;
-        distance_ = Swap(s_, distance_);
+        Swap(s_);
         break;
       case 2:
         //cout << "2-opt" << endl;
-        distance_ = _2opt(s_, distance_);
+        _2opt(s_);
         break;
       case 3:
         //cout << "Reinsertion" << endl;
-        distance_ = orkOpt(s_, distance_, 1);
+        orkOpt(s_, 1);
         break;
       case 4:
         //cout << "Or-2-opt" << endl;
-        distance_ = orkOpt(s_, distance_, 2);
+        orkOpt(s_, 2);
         break;
       case 5:
         //cout << "Or-3-opt" << endl;
-        distance_ = orkOpt(s_, distance_, 3);
+        orkOpt(s_, 3);
         break;
     }
-    if(distance_ < distance){
+    if(s_.distance < s.distance){
       s = s_;
-      distance = distance_;
       NL.clear();
       for (int i=1; i<=5; ++i) NL.push_back(i);
       random_shuffle(NL.begin(), NL.end());
@@ -299,8 +295,6 @@ int RVND(vector<int> &s){
       NL.erase(NL.begin());
     }
   }
-
-  return distance;
 }
 
 
@@ -308,11 +302,11 @@ int RVND(vector<int> &s){
 int main(int argc, char** argv) {
 
   double alpha;
-  vector<int>  s;
-  int distance = 0, distance_ = 0;
-  vector<int>  s_;
+  solution  s;
+  solution  s_;
   int f = 10000000;
-  vector<int> bestRoute;
+  solution bestRoute;
+  
 
   /* Generate seed randomly*/
   srand (time(NULL));
@@ -325,30 +319,31 @@ int main(int argc, char** argv) {
     IILS = dimension;
   }
   
+  
   // Get starting timepoint 
   auto start = high_resolution_clock::now(); 
   //printData();
   for(int i = 0; i < IMAX; i++){
     alpha = R[rand()%R.size()];
     auto inicio = high_resolution_clock::now(); 
-    s = Construction(alpha);
+    s.cities = Construction(alpha);
+    s.distance = getDistance(s.cities);
     auto fim = high_resolution_clock::now(); 
     auto duracao = duration_cast<microseconds>(fim - inicio); 
     //cout << "Construção: " << duracao.count()/1000000.0 << endl;
     s_ = s;
-    distance_ = getDistance(s_);
     for(int iterILS = 0; iterILS < IILS; iterILS++){
-      distance = RVND(s);
-      if(distance < distance_){
+      RVND(s);
+      if(s.distance < s_.distance){
         s_ = s;
-        distance_ = distance;
         iterILS = 0;
       }
       s = Pertub(s_);
+      s.distance = getDistance(s.cities);
     }
-    if(distance_ < f){
+    if(s_.distance < f){
       bestRoute = s_;
-      f = distance_;
+      f = s_.distance;
     }
   }
   // Get ending timepoint 
